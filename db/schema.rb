c0348f7ec8c814e5973709f49131da9571f125b5
Erin Hinson <hinson.erin@gmail.com>
2015-04-27 21:03:17 -0400

    Add Collection Id to Users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150428010108) do
+ActiveRecord::Schema.define(version: 20150428010230) do
 
   create_table "collections", force: :cascade do |t|
     t.integer  "user_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150428010108) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "collection_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
