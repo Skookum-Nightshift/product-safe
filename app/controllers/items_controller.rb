3f38a37ab45bfae125822e181760de444a413a27
Erin Hinson <hinson.erin@gmail.com>
2015-05-03 18:00:31 -0400

    Item Search from Amazon API

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < InheritedResources::Base
     def index
       @items = Item.all
 
-      
+      @item = item
     end
     def item_params
       params.require(:item).permit(:collection_id, :expiration_date, :image, :barcode, :expiration_date, :amazon_url, :name)
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,23 +1,25 @@
 class SearchController < ApplicationController
   respond_to :json
 
-  def item_search
-    api_request = Vacuum.new
+  def item_search(item)
+    request = Vacuum.new
 
-    api_request.configure(
-      aws_access_key_id: ENV["AWS_KEY"],
-      aws_secret_access_key: ENV["AWS_SECRET"],
-      associate_tag: ENV["AWS_ASSOCIATE_TAG"]
+    request.configure(
+      aws_access_key_id: ENV["S3_KEY_ID"],
+      aws_secret_access_key: ENV["S3_SECRET"],
+      associate_tag: "tag"
     )
 
     response = request.item_lookup(
       query: {
-        'ItemId' => '#{@item.barcode}',
+        'ItemId' => '076243631X',
         'IdType' => 'ISBN',
         'SearchIndex' => 'All'
       }
     )
 
-    respond_with results.as_json
+
+    response.to_h["ItemLookupResponse"]["Items"]["Request"]["IsValid"]
+
   end
 end
